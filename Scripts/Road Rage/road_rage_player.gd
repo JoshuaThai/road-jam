de303208f9375556bb818da0d831603e430ba650
JoshuaThai <joshua.thaisananikone@gmail.com>
2026-01-31 15:41:57 -0600

    Experimented with Camera in Car

---
+++ Scripts/Road Rage/road_rage_player.gd
@@ -0,0 +1,21 @@
+extends Camera3D
+
+# Hide cursor in game
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+# Player will rotate with mouse motion!
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= event.relative.x * 0.5
+#		Prevents players from rotating 360 view
+		rotation_degrees.y = clamp(
+			rotation_degrees.y, -225.0, -135.0
+		)
+		rotation_degrees.x -= event.relative.y * 0.2
+#		Prevent players from fipping the view.
+		rotation_degrees.x = clamp(
+			rotation_degrees.x, -80.0, 80.0
+		)
+#		Show cursor when player clicks escape key.
+	elif event.is_action_pressed("ui_cancel"): 
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
