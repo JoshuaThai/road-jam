dc8f65d0f524b897fc0e7a9219e78a56000118a2
JoshuaThai <joshua.thaisananikone@gmail.com>
2026-02-07 14:35:40 -0600

    Modified car.
    Added merging lanes ability to car. Also made the car be a vehicle that you can drive yourself.

--- Joshua/Scripts/Road Rage/road_rage_car.gd
+++ Joshua/Scripts/Road Rage/road_rage_car.gd
@@ -2,6 +2,19 @@ extends VehicleBody3D
 
 const SPEED = 40
 
+# Car variables
+# Prevent the car from moving at the start
+var carStarted = false
+# Keep track of when the car is accelerating
+var isAccelerating = false
+# Keep track of when the car is merging
+var isMerging = false
+# Make sure the car can only merge between the left and right lane
+var inLeft = false
+
+var speedOffset = 0
+
+@onready var CarAnimations = $CarAnimations
 @onready var rear_mirror = $Mirrors/RearViewport/MirrorCamera
 @onready var left_mirror = $Mirrors/LeftViewport/MirrorCamera
 @onready var right_mirror = $Mirrors/RightViewport/MirrorCamera
@@ -16,7 +29,45 @@ func _process(_dt):
 	right_mirror.global_transform = right_marker.global_transform
 
 func _physics_process(delta):
-	global_position.z += SPEED * delta
+
+		#CarAnimations.play("move_left")
+	# When player are clicking w, they are moving forward.
+	if (Input.is_key_pressed(Key.KEY_W)):
+		if(!carStarted):
+			carStarted = true
+		speedOffset = 0
+		isAccelerating = true
+	else:
+		isAccelerating = false
+	if isAccelerating:
+#		YOU CAN ONLY SWITCH LANES WHILE ACCELERATING
+		#	If you press q, player switch lanes to left.
+		if(Input.is_key_pressed(Key.KEY_Q) and not isMerging and not inLeft):
+			inLeft = true
+			isMerging = true
+			var tween = create_tween()
+			#var newPosition = Vector3(global_position.x + 5, global_position.y, global_position.z)
+			tween.tween_property(self, "global_position:x", global_position.x + 5, 1)
+			isMerging = false
+			
+#			If you press E, player switch lane to the right.
+		if(Input.is_key_pressed(Key.KEY_E) and not isMerging and inLeft):
+			inLeft = false
+			isMerging = true
+			var tween = create_tween()
+			#var newPosition = Vector3(global_position.x + 5, global_position.y, global_position.z)
+			tween.tween_property(self, "global_position:x", global_position.x - 5, 1)
+			isMerging = false
+		global_position.z += SPEED * delta
+	else:
+		if(!carStarted): return
+#		Player should gradually slow down if not accelerating.
+		var speed = (SPEED * delta) - speedOffset
+		speedOffset += 0.005
+		if speed <= 0: 
+			global_position.z += 0
+		else:
+			global_position.z += speed
 
 
 
